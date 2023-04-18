b3807758e3bbdd0357e023868be50987de4534e9
AndresAlvarado9 <65028395+AndresAlvarado9@users.noreply.github.com>
2023-04-18 11:28:26 -0500

    Colocación de mapa y script en prueba

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+function initMap() {
+  var location = {lat: 40.712776, lng: -74.005974}; // Cambia las coordenadas por las de tu ubicación
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: location
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
