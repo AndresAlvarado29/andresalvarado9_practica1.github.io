347de64256c9350ac0b4b65061a96063c9cd698a
AndresAlvarado9 <65028395+AndresAlvarado9@users.noreply.github.com>
2023-04-19 08:30:08 -0500

    se agrego animaciones y tranciciones a imagenes

---
+++ script.js
@@ -0,0 +1,12 @@
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("La geolocalización no está soportada por este navegador.");
+  }
+}
+
+function showPosition(position) {
+  console.log("Latitud: " + position.coords.latitude +
+  " Longitud: " + position.coords.longitude);
+}
